c9927863c6204fbf107c04e6e6ccc91a9cb54330
Evgeniy Serov <kejik148@gmail.com>
2025-12-29 14:55:13 +0500

    Refactoring World and its services

--- Scenes/World/Services/StartStop/WorldTreeLoadService.cs
+++ Scenes/World/Services/StartStop/WorldTreeLoadService.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
-namespace GodotTemplate.Scenes.World.StartStop;
+namespace GodotTemplate.Scenes.World.Services.StartStop;
 
 public class WorldTreeLoadService
 {
---
+++ Scenes/World/Services/WorldSynchronizerService.cs
@@ -0,0 +1,8 @@
+﻿using Godot;
+
+namespace GodotTemplate.Scenes.World.Services;
+
+public partial class WorldSynchronizerService : Node
+{
+    //TODO Перенести сюда синхронайзер, или основную логику из него. Ивенты оставить в старом/здесь или WorldEvent (пусть тоже будет сервис + RPC ивенты??)
+}
