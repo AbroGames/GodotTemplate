7e830363b0f15a27a1f9f6347132a8a367c84bf1
Evgeniy Serov <kejik148@gmail.com>
2026-02-11 22:57:53 +0500

    Try test memory leak

--- Scenes/Game/Starters/ConnectToMultiplayerGameStarter.cs
+++ Scenes/Game/Starters/ConnectToMultiplayerGameStarter.cs
@@ -1,6 +1,6 @@
 ﻿using Godot;
+using GodotTemplate.Scenes.World.Services;
 using GodotTemplate.Scripts.Content.LoadingScreen;
-using GodotTemplate.Scripts.Service.Settings;
 
 namespace GodotTemplate.Scenes.Game.Starters;
 
@@ -15,16 +15,16 @@ public class ConnectToMultiplayerGameStarter(string host = null, int? port = nul
         base.Init(game);
         Services.LoadingScreen.SetLoadingScreen(LoadingScreenTypes.Type.Connecting);
         
-        PlayerSettings playerSettings = Services.PlayerSettings.GetPlayerSettings();
-        game.AddWorld();
-        Synchronizer synchronizer = game.AddSynchronizer(playerSettings);
-        ConnectToClientSynchronizerEvents(synchronizer);
+        World.World world = game.AddWorld();
         game.AddHud();
         Network.Network network = game.AddNetwork();
+        ConnectToClientSynchronizerEvents(world.SynchronizerService);
         
-        game.GetMultiplayer().ConnectedToServer += synchronizer.StartSyncOnClient;
+        game.GetMultiplayer().ConnectedToServer += () => StartSyncOnClient(world.SynchronizerService);
         game.GetMultiplayer().ConnectionFailed += ConnectionFailedEvent;
         game.GetMultiplayer().ServerDisconnected += ServerDisconnectedEvent;
+        
+        //AddMultiplayerEventListeners(game, world.SynchronizerService); TODO
         
         Error error = network.ConnectToServer(host ?? DefaultHost, port ?? DefaultPort);
         if (error != Error.Ok)
@@ -32,10 +32,41 @@ public class ConnectToMultiplayerGameStarter(string host = null, int? port = nul
             ConnectionFailedEvent();
         }
     }
-
+    
     // Failed attempt to connect to the server (did not receive a response from the server within the timeout).
     private void ConnectionFailedEvent() => GoToMenuAndShowError(ConnectionFailedMessage);
     
     // Server disconnected (the connection was successful, but the server disconnected us). This may also happen several hours after the connection.
     private void ServerDisconnectedEvent() => GoToMenuAndShowError(DisconnectedFromServerMessage);
+    
+    private void AddMultiplayerEventListeners(Game game, WorldSynchronizerService synchronizerService)
+    {
+        void ConnectionSuccessEvent() => StartSyncOnClient(synchronizerService);
+        
+        // Failed attempt to connect to the server (did not receive a response from the server within the timeout).
+        void ConnectionFailedEvent()
+        {
+            GD.Print("Connection failed"); //TODO
+            GoToMenuAndShowError(ConnectionFailedMessage);
+            RemoveMultiplayerEventListeners();
+        }
+        
+        // Server disconnected (the connection was successful, but the server disconnected us). This may also happen several hours after the connection.
+        void ServerDisconnectedEvent()
+        {
+            GoToMenuAndShowError(DisconnectedFromServerMessage);
+            RemoveMultiplayerEventListeners();
+        }
+        
+        void RemoveMultiplayerEventListeners()
+        {
+            game.GetMultiplayer().ConnectedToServer -= ConnectionSuccessEvent;
+            game.GetMultiplayer().ConnectionFailed -= ConnectionFailedEvent;
+            game.GetMultiplayer().ServerDisconnected -= ServerDisconnectedEvent;
+        }
+        
+        game.GetMultiplayer().ConnectedToServer += ConnectionSuccessEvent;
+        game.GetMultiplayer().ConnectionFailed += ConnectionFailedEvent;
+        game.GetMultiplayer().ServerDisconnected += ServerDisconnectedEvent;
+    }
 }
