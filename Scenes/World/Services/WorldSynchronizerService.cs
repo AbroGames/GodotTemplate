66160c98a04710a4347ea350a2fd2b1523696a06
Evgeniy Serov <kejik148@gmail.com>
2026-02-23 00:30:21 +0500

    Refactoring WorldSynchronizerService

--- Scenes/World/Services/WorldSynchronizerService.cs
+++ Scenes/World/Services/WorldSynchronizerService.cs
@@ -22,9 +22,7 @@ public partial class WorldSynchronizerService : Node
     private const string NicknameAlreadyUsedErrorMessage = "Nickname is already used";
     
     public event Action SyncStartedOnClientEvent;
-    public event Action<int> SyncEndedOnServerEvent;
     public event Action SyncEndedOnClientEvent;
-    public event Action<string> SyncRejectOnServerEvent;
     public event Action<string> SyncRejectOnClientEvent;
 
     [SceneService] private WorldPersistenceData _persistenceData;
@@ -39,7 +37,7 @@ public partial class WorldSynchronizerService : Node
 
     public void StartSyncOnClient(string nick, Color color)
     {
-        _log.Information("Starting sync with server with nick '{nick}'", nick);
+        _log.Information("Starting sync with server as '{nick}'", nick);
         SyncStartedOnClientEvent?.Invoke();
         NewClientInitOnServer(nick, color);
     }
@@ -49,25 +47,22 @@ public partial class WorldSynchronizerService : Node
     private void NewClientInitOnServerRpc(string nick, Color color)
     {
         int connectedClientId = GetMultiplayer().GetRemoteSenderId();
-        _log.Information("Peer {peer} try sync with server with nick '{nick}'", connectedClientId, nick);
+        _log.Information("Peer {peer} attempting to sync using nick '{nick}'", connectedClientId, nick);
 
         if (_temporaryData.PlayerNickByPeerId.Values.Contains(nick))
         {
             _log.Warning("Syncing peer {peer} was rejected with error: {error}", connectedClientId, NicknameAlreadyUsedErrorMessage);
-            SyncRejectOnServerEvent?.Invoke(NicknameAlreadyUsedErrorMessage);
             RejectSyncOnClient(connectedClientId, NicknameAlreadyUsedErrorMessage);
         }
         if (nick.Length < NicknameMinLength || nick.Length > NicknameMaxLength)
         {
             _log.Warning("Syncing peer {peer} was rejected with error: {error}", connectedClientId, LengthOfNicknameErrorMessage);
-            SyncRejectOnServerEvent?.Invoke(LengthOfNicknameErrorMessage);
             RejectSyncOnClient(connectedClientId, LengthOfNicknameErrorMessage);
         }
         _temporaryData.PlayerNickByPeerId.Add(connectedClientId, nick);
 
         if (!_persistenceData.Players.PlayerByNick.ContainsKey(nick))
         {
-            _log.Information("Add peer {peer} as new player", connectedClientId);
             _persistenceData.Players.AddPlayer(new PlayerData
             {
                 Nick = nick
@@ -76,11 +71,10 @@ public partial class WorldSynchronizerService : Node
         PlayerData playerData = _persistenceData.Players.PlayerByNick[nick];
         playerData.Color = color;
         playerData.IsAdmin = nick.Equals(_temporaryData.MainAdminNick);
-        _log.Information("Player data about peer {peer} was synced ", connectedClientId);
+        _log.Information("Player data from peer {peer} was synced", connectedClientId);
         
-        _log.Information("Send to peer {peer} serialized world data", connectedClientId);
+        _log.Information("Sending serialized world data to peer {peer}", connectedClientId);
         EndSyncOnClient(connectedClientId, _dataSerializerService.SerializeWorldData());
-        SyncEndedOnServerEvent?.Invoke(connectedClientId);
     }
 
     private void EndSyncOnClient(long peerId, byte[] serializableData) => RpcId(peerId, MethodName.EndSyncOnClientRpc, serializableData);
@@ -90,17 +84,13 @@ public partial class WorldSynchronizerService : Node
         _log.Information("Received serialized world data from server");
         _dataSerializerService.DeserializeWorldData(serializableData);
         SyncEndedOnClientEvent?.Invoke();
-        
-        //TODO Создать два StartStop сервиса: клиент и сервер? Из GameStarter дергать его (у клиента и сервера), а с него у клиента уже синхронайзер дергать. С него же пинг запускать.
-        // Или пинг от ивента синхронайзера? Но привязка в StartStop по идее
-        GetParent<World>().PerformanceService.Ping.Start();
     }
     
     private void RejectSyncOnClient(long peerId, string errorMessage) => RpcId(peerId, MethodName.RejectSyncOnClientRpc, errorMessage);
     [Rpc(CallLocal = true)] 
     private void RejectSyncOnClientRpc(string errorMessage)
     {
-        _log.Error("Synchronization with the server was rejected: {error}", errorMessage);
+        _log.Error("Syncing with the server was rejected with error: {error}", errorMessage);
         SyncRejectOnClientEvent?.Invoke(errorMessage);
     }
 }
