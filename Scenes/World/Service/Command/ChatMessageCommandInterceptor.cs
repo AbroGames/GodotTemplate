2090ca9a6a36402aa9c5dbb85c6b7271f8420d5c
Evgeniy Serov <kejik148@gmail.com>
2026-03-12 17:19:54 +0500

    Add chat command service

---
+++ Scenes/World/Service/Command/ChatMessageCommandInterceptor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using GodotTemplate.Scenes.World.Service.Chat;
+
+namespace GodotTemplate.Scenes.World.Service.Command;
+
+public class ChatMessageCommandInterceptor : IChatMessageInterceptor
+{
+    private readonly Action<int, string> _onCommand;
+
+    public ChatMessageCommandInterceptor(Action<int, string> onCommand)
+    {
+        _onCommand = onCommand;
+    }
+
+    public bool IsPass(int senderId, string text)
+    {
+        if (text.StartsWith('/'))
+        {
+            _onCommand.Invoke(senderId, text.Substring(1));
+            return false;
+        }
+        return true;
+    }
+}
