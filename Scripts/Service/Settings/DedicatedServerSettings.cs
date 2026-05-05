e78f1923d38be284f8ab2cf072bdca0f7f3a326d
Evgeniy Serov <kejik148@gmail.com>
2026-05-05 14:26:56 +0500

    Fix bug with auto-save in dedicated server

---
+++ Scripts/Service/Settings/DedicatedServerSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace GodotTemplate.Scripts.Service.Settings;
+
+public record DedicatedServerSettings(
+    string Locale,
+    bool AutoSaveEnabled
+)
+{
+    public static DedicatedServerSettings GetDefault()
+    {
+        return new(
+            Locale: Services.I18N.GetUserOsLocaleInfoOrDefault().Code,
+            AutoSaveEnabled: true
+        );
+    }
+}
